275c3934ac32ecb40a42867b92f00c1d0cd91fc4
Aleksandr Provarenko <provar67@gmail.com>
2023-01-05 13:03:22 +0300

    Displayed SideMenu in ContentView

--- TwitterClone/ContentView.swift
+++ TwitterClone/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showMenu = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .topLeading) {
+            MainTabBarView()
+                .navigationBarHidden(showMenu)
+            
+            if showMenu {
+                ZStack {
+                    Color(.black).opacity(0.25)
+                }.onTapGesture {
+                    withAnimation(.easeInOut) {
+                        showMenu = false
+                    }
+                }
+                .ignoresSafeArea()
+            }
+            
+            SideMenuView()
+                .frame(width: 300)
+                .offset(x: showMenu ? 0: -300, y: 0)
+                .background(showMenu ? Color.white : Color.clear)
         }
-        .padding()
+        .navigationTitle("home")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    withAnimation(.easeInOut) {
+                        showMenu.toggle()
+                    }
+                } label: {
+                    Circle()
+                        .frame(width: 35, height: 35)
+                }
+
+            }
+        }
     }
 }
 
