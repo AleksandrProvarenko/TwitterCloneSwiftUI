b77f424c06d7943eaecc6fc4084e8d5980c23b11
Aleksandr Provarenko <provar67@gmail.com>
2023-01-05 18:39:44 +0300

    Created NewTweetView

---
+++ TwitterClone/Core/UploadTweet/View/NewTweetView.swift
@@ -0,0 +1,57 @@
+//
+//  NewTweetView.swift
+//  TwitterClone
+//
+//  Created by Alex Provarenko on 05.01.2023.
+//
+
+import SwiftUI
+
+struct NewTweetView: View {
+    
+    @State private var caption = ""
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(Color(.systemBlue))
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("Tweet")
+                } label: {
+                    Text("Tweet")
+                        .bold()
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+            }
+            .padding()
+            
+            HStack(alignment: .top) {
+                Circle()
+                    .frame(width: 65, height: 65)
+                
+                TextArea("What's happening?", text: $caption)
+            }
+            .padding()
+        }
+    }
+}
+
+
+struct NewTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTweetView()
+    }
+}
